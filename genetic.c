d4f620870a7c8186bc5b680fd0e5e819380709cb
Tecnarca <mail.tecnarca@gmail.com>
2018-10-12 17:09:31 +0200

    Changed variable names and comments to english

--- genetic.c
+++ genetic.c
@@ -4,103 +4,103 @@
 #include <math.h>
  
 #define DEBUG 0
-#define X_MAX 700                   //Dimensione griglia
+#define X_MAX 700                   //Grid dimensions
 #define Y_MAX 700
-#define CITY_COUNT 25               //Numero di città
-#define POPULATION_COUNT 256        //Numero di individui
-#define ITERATION_COUNT 10000       //Numero di generazioni
-#define MUTATION_LIKELIHOOD 0.25    //probabilità di mutazione in [0,1]
+#define CITY_COUNT 25               //Number of cities
+#define POPULATION_COUNT 256        //Number of individuals
+#define ITERATION_COUNT 10000       //Max number of iterations allowed
+#define MUTATION_LIKELIHOOD 0.25    //Random mutation probability
  
 /*-------------------------------------------------------------*/
  
-typedef struct{                 //Coordinate città
+typedef struct{                 //Contains the coords of a city within the grid
     unsigned x, y;
 } city;
  
-typedef struct{                 //Struttura individuo
-    int solution[CITY_COUNT];   //Vettore solution, ovvero l'ordine di visita delle città
-    double fitness;             //Bontà della soluzione
+typedef struct{                 //Strucure of an individual
+    int solution[CITY_COUNT];   //Solution vector, the cities are traversed in order
+    double fitness;             //Quantifies the goodness of the above solution
 } individual;
  
 /*-------------------------------------------------------------*/
  
 void debug(char *);
-double min(individual *);
-void setup_cities(city *);
-void generate_initial_population(individual *);
-double get_distance(city *, int, int);
-double * evaluate_fitness(city *, individual *);
-int * selection(double *);
-individual * crossover(individual *, int *);
-void mutation(individual *);
-void cycle_crossover(individual *, int, int, int *, int *);
-void scambia(int *, int *);
-int highest_fitness(individual * population);
+double min(individual *); 
+void setup_cities(city *);                                      // Generates randomically the cities positions
+void generate_initial_population(individual *);                 // Generates casually the initial population, creating random solutions
+double get_distance(city *, int, int);                          // Returns the distance between two cities, given a cities_map
+double * evaluate_fitness(city *, individual *);                // Computes the various fitness values for every individual in the input, given a cities_map
+int * selection(double *);                                      // 
+individual * crossover(individual *, int *);                    // 
+void mutation(individual *);                                    // Randomically changes some solutions (swapping two elements within a solution) based on the likelihood
+void cycle_crossover(individual *, int, int, int *, int *);     // Computes the cycle crossover alghoritm generating two new solutions (int *), given two parent IDs (int, index of individual *)
+void swap(int *, int *);                                        // Swaps two cities within a solution
+int highest_fitness(individual *);                              // Returns the index of the individual with the best fitness value
  
 /*-------------------------------------------------------------*/
  
 int main(){
  
-    srand(time(NULL));           //Inizializzazione del generatore di numeri pseudo casuali
+    srand(time(NULL));           //Random seed initialization
  
-    city cities_map[CITY_COUNT]; //Mappa delle città
+    city cities_map[CITY_COUNT]; //Allocates the cities in the maps
  
     individual * population, * new_gen;
  
-    population = malloc(sizeof(individual) * POPULATION_COUNT); //population contiene POPULATION_COUNT individui
+    population = malloc(sizeof(individual) * POPULATION_COUNT); //Allocates the population, there are "POPULATION_COUNT" individuals
  
-    setup_cities(cities_map);       //Generazione iniziale della mappa
+    setup_cities(cities_map);       //Generates (randomically) the cities position
  
     FILE * fd;
  
-    fd = fopen("data.txt", "w");    //Questo file contiene le informazioni da utilizzare per la grafica
+    fd = fopen("data.txt", "w");    //this file will be used in the graphical script to show the various generations
     if (fd == NULL){
-        printf("Errore apertura file\n");
+        printf("Fatal error: could not open the data file\n");
         return -1;
     }
  
-    fprintf(fd, "%d\n", CITY_COUNT);        //Salviamo il numero di città
+    fprintf(fd, "%d\n", CITY_COUNT);        //We save the number of cities generated...
  
-    for (int i = 0; i < CITY_COUNT; i++){   //E le loro coordinate
+    for (int i = 0; i < CITY_COUNT; i++){   //...and the various coordinates
         fprintf(fd, "%d,%d\n", cities_map[i].x, cities_map[i].y);
     }
  
-    generate_initial_population(population);    //Genera la popolazione iniziale, partiamo da soluzioni casuali per ogni individuo
+    generate_initial_population(population);    //Generates casually the initial population, creating random solutions
  
     int best;
-    int * candidati;
+    int * candidates;
     double length;
-    double * ripartizione;
+    double * cdf;
  
     for (int i = 0; i < ITERATION_COUNT; i++){
  
-        ripartizione = evaluate_fitness(cities_map, population);    //Otteniamo la distribuzione di probabilità (ripartizione) associata alla popolazione corrente
+        cdf = evaluate_fitness(cities_map, population);    //We obtain the cumulative distribution function associated to the current population
  
         if (i%100 == 0){
  
-            best = highest_fitness(population);     //Troviamo l'indice dell'individuo con la soluzione migliore
+            best = highest_fitness(population);     //Returns the index of the individual with the best fitness value
            
-            length = 1/population[best].fitness;    //la lunghezza del percorso è 1/fitness essendo fitness calcolato come 1/lunghezza
+            length = 1/population[best].fitness;    //The lenght of the solution is the inverse of the fitness (the fitness is defined as 1/lenght)
            
-            fprintf(fd, "%d %f ", i, length);       //Salviamo la lunghezza del percorso
+            fprintf(fd, "%d %f ", i, length);       //Save the best lenght...
  
             for (int j = 0; j < CITY_COUNT; j++){
-                fprintf(fd, "%d ", population[best].solution[j]);   //E l'ordine delle città
+                fprintf(fd, "%d ", population[best].solution[j]);   //... and the solution
             }
             fprintf(fd, "\n");
  
-            printf("Generazione attuale: %d | Distanza migliore: %f\n", i, length);
+            printf("Current generation: %d | Better length: %f\n", i, length);
         }
  
-        candidati = selection(ripartizione);        //canditati è un vettore di interi contenente gli indici degli individui scelti secondo la distribuzione ripartizione
+        candidates = selection(cdf);        //candidates is an integer vector containing the indexes of those that are going to reproduce
        
-        new_gen = crossover(population, candidati); //Incrociamo gli individui ottenendo la nuova popolazione new_gen
+        new_gen = crossover(population, candidates); //We crossover the population to obtain a new one
  
         free(population);
  
         population = new_gen;
  
-        mutation(population);   //Applichiamo la mutazione sulla popolazione
+        mutation(population);   //We apply the eventual mutation to the population
     }
  
     fclose(fd);
@@ -114,7 +114,7 @@ void debug(char *message){
     if (DEBUG) printf("[D] %s\n", message);
 }
  
-void scambia(int * a, int * b){
+void swap(int * a, int * b){
     int t = *a;
     *a = *b;
     *b = t;
@@ -124,8 +124,8 @@ int highest_fitness(individual * population){
  
     int index = 0;
     double initial = population[0].fitness;
-    for (int j = 0; j < POPULATION_COUNT; j++){
-        if (population[j].fitness > initial){   //Troviamo il fitness massimo e restituiamo l'indice
+    for (int j = 1; j < POPULATION_COUNT; j++){
+        if (population[j].fitness > initial){   //Finds the highest fitness
             initial = population[j].fitness;
             index = j;
         }
@@ -136,11 +136,11 @@ int highest_fitness(individual * population){
 void setup_cities(city * cities_map){
  
     for (int i = 0; i < CITY_COUNT; i++){
-        cities_map[i].x = rand() % X_MAX;   //Genera le coordinate per ogni città in [0, X_MAX) e [0, Y_MAX)
+        cities_map[i].x = rand() % X_MAX;   //Generates the coordinates for the cities between [0, X_MAX) and [0, Y_MAX)
         cities_map[i].y = rand() % Y_MAX;
     }
  
-    debug("Generazione della mappa completata");
+    debug("Random map generation completed");
 }
  
 void generate_initial_population(individual * population){
@@ -149,15 +149,15 @@ void generate_initial_population(individual * population){
  
     for (int i = 0; i < POPULATION_COUNT; i++){
         for (int j = 0; j < CITY_COUNT; j++){
-            x = rand() % (j+1);     //Numero casuale in [0, j]
-            if (x != j){            //All'elemento alla posizione j viene assegnato l'elemento alla posizione x (se x strettamente minore di j altrimenti assegneremmo a solution[j] un valore non inizializzato)
+            x = rand() % (j+1);     //Casual number between [0, j]
+            if (x != j){            //The element at position X gets assigned to the element at position J if x<j, otherwise solution[j] is unitizialized
                 population[i].solution[j] = population[i].solution[x];
             }
             population[i].solution[x] = j;
         }
-    }
+    } 
  
-    debug("Generazione della popolazione iniziale completata");
+    debug("Random population generation completed");
 }
  
 double get_distance(city * cities_map, int id_1, int id_2){
@@ -175,75 +175,75 @@ double * evaluate_fitness(city * cities_map, individual * population){
  
     int id_1, id_2;
     double total, fitness_sum = 0;
-    double * ripartizione = malloc(sizeof(double) * POPULATION_COUNT);  //Allochiamo spazio per la distribuzione di probabilità
+    double * cdf = malloc(sizeof(double) * POPULATION_COUNT);  //Allocate space for the cumulative distribution function of a probability distribution
  
     for (int i = 0; i < POPULATION_COUNT; i++){
  
-        total = 0;  //Contiene la distanza totale tra le città
+        total = 0;  //Contains the total distances of one solution
  
         for (int j = 0; j < CITY_COUNT-1; j++){
             id_1 = population[i].solution[j];
             id_2 = population[i].solution[j+1];
-            total += get_distance(cities_map, id_1, id_2);  //Calcola la distanza tra due città e la somma alla totale
+            total += get_distance(cities_map, id_1, id_2);  //Caomputes the distance between cities and the total sum
         }
  
-        population[i].fitness = 1/total;        //A questo punto il fitness è 1/distanza totale
-        fitness_sum += population[i].fitness;   //La somma dei fitness viene utilizzata per la "normalizzazione"
+        population[i].fitness = 1/total;        //The fitness is 1/total
+        fitness_sum += population[i].fitness;   //We use the sum of all the fitness to normalize the probability distribution
     }
  
-    ripartizione[0] = population[0].fitness/fitness_sum;
+    cdf[0] = population[0].fitness/fitness_sum;
        
     for (int i = 1; i < POPULATION_COUNT; i++){
-        ripartizione[i] = ripartizione[i-1] + population[i].fitness/fitness_sum;    //Assegna al valore ripartizione il valore dell'elemento precedente + il fitness corrente normalizzato (diviso per la loro somma)
+        cdf[i] = cdf[i-1] + population[i].fitness/fitness_sum;    //Assign to cumulative distribution the value of the previous element + the current normalized fitness divided by the sum of all the fitnesses
     }
  
-    debug("Fitness calcolati");
+    debug("Computed fitness");
    
-    return ripartizione;
+    return cdf;
 }
  
-int * selection(double * ripartizione){
+int * selection(double * cdf){
  
     double extraction;
     int pivot, first, last;
-    int * candidati = malloc(sizeof(int) * POPULATION_COUNT);
+    int * candidates = malloc(sizeof(int) * POPULATION_COUNT);
  
     for (int i = 0; i < POPULATION_COUNT; i++){
  
         first = 0;
         last = POPULATION_COUNT;
-        extraction = (double)rand()/RAND_MAX;   //rand() genera un numero in [0, RAND_MAX] quindi dividendo per RAND_MAX otteniamo un numero in [0, 1]
+        extraction = (double)rand()/RAND_MAX;   //rand() generates a number in [0, RAND_MAX] therefore dividing by RAND_MAX we obtain a value in [0, 1]
  
-        while (first != last){  //Eseguiamo una ricerca binaria del primo elemento del vettore ripartizione con probabilità maggiore di quella estratta
+        while (first != last){  //Binary search of the first element of the CDF that has bigger probability than the value randomly generated
             pivot = (first + last)/2;
-            if (ripartizione[pivot] == extraction){
+            if (cdf[pivot] == extraction){
                 break;
             }
-            if (ripartizione[pivot] > extraction){
+            if (cdf[pivot] > extraction){
                 last = pivot;
             } else {
                 first = pivot + 1;
             }
         }
-        candidati[i] = first; //candidati[i] contiene l'indice dell'individuo estratto, maggiore è la sua probabilità in ripartizione, maggiore è la probabilità di essere in candidati, anche ripetutamente
+        candidates[i] = first; //candidates[i] contains the index of the extracted individual, the bigger the probability, the more the index appears in candidates (so it reproduces often because it's a good solution)
     }
  
-    free(ripartizione); //Non serve più a questo punto
+    free(cdf); //It does not serve any purpose anymore
  
-    debug("Candidati estratti");
+    debug("Extracted candidates");
    
-    return candidati;
+    return candidates;
 }
  
-individual * crossover(individual * population, int * candidati){
+individual * crossover(individual * population, int * candidates){
  
     individual * new_gen = malloc(sizeof(individual) * POPULATION_COUNT);
  
     for (int i = 0; i < POPULATION_COUNT; i+=2){
-        cycle_crossover(population, candidati[i], candidati[i+1], new_gen[i].solution, new_gen[i+1].solution);  //Eseguiamo il crossover a due a due scegliendo come genitori due candidati scelti tramite l'indice, salviamo i figli in new_gen
+        cycle_crossover(population, candidates[i], candidates[i+1], new_gen[i].solution, new_gen[i+1].solution);  //Eseguiamo il crossover a due a due scegliendo come genitori due candidates scelti tramite l'indice, salviamo i figli in new_gen
     }
  
-    free(candidati);    //Non servono più
+    free(candidates);    //Non servono più
  
     debug("Riproduzione effettuata");
  
@@ -256,46 +256,46 @@ void mutation(individual * population){
  
     for (int i = 0; i < POPULATION_COUNT; i++){
  
-        if (rand() < p){    //In questo caso p = RAND_MAX*0.25 = RAND_MAX/4, poiché rand() genera un numero in [0, RAND_MAX] la condizione rand() < p ha 1 probabilità su 4 di verificarsi
+        if (rand() < p){    //In this case p = RAND_MAX*0.25 = RAND_MAX/4, because rand() generates a number in [0, RAND_MAX]  and therefore this condition has probability 1/4 of being true
             do {
                 x = rand() % CITY_COUNT;
                 y = rand() % CITY_COUNT;
-            } while (x == y);   //evitiamo che x sia uguale a y
+            } while (x == y);   //we should not commute x with itself, it's useless
  
-            scambia(&population[i].solution[x], &population[i].solution[y]);    //Scambiamo le 2 città
+            scambia(&population[i].solution[x], &population[i].solution[y]);    //Swap the cities
         }
     }
  
-    debug("Mutazioni eseguite");
+    debug("Mutation executed");
 }
  
 void cycle_crossover(individual * population, int id_1, int id_2, int * child_1, int * child_2){
  
-    int parent_1 = id_1, parent_2 = id_2;           //Copiamo gli indici dei genitori
-    int j = 0;                                      //Colonna corrente
-    int lookup[CITY_COUNT];                         //Vettore di indirizzamento
-    int flags[CITY_COUNT] = {0};                    //Vettore delle colonne visitate
-    int visited = 0;                                //Numero di colonne visitate
-    int first = population[id_1].solution[0], last; //primo e ultimo elemento del ciclo
+    int parent_1 = id_1, parent_2 = id_2;           //Copy the parent indexes
+    int j = 0;                                      //Current column
+    int lookup[CITY_COUNT];                         //Addressing vector
+    int flags[CITY_COUNT] = {0};                    //Visited columns vector
+    int visited = 0;                                //number of visited columns
+    int first = population[id_1].solution[0], last; //First and last element of the cycle
  
     for (int i = 0; i < CITY_COUNT; i++){
-        lookup[population[id_1].solution[i]] = i;   //Questo vettore viene utilizzato per ottenere la posizione di un elemento del vettore id_2 nel vettore id_1, per farlo salviamo in lookup[i] la posizione dell'elemento da cercare
+        lookup[population[id_1].solution[i]] = i;   //this vector is used to obtain the position of an element of id_2 in id_1, to do this we save in lookup[i] the position of the element to search
     }
  
     while (visited < CITY_COUNT){
  
-        child_1[j] = population[parent_1].solution[j];  //Copiamo le città nei figli
+        child_1[j] = population[parent_1].solution[j];  //We copy the cities in the childs
         child_2[j] = population[parent_2].solution[j];
         last = population[id_2].solution[j];
  
-        flags[j] = 1;       //Impostiamo la colonna j come visitata
+        flags[j] = 1;       //J becomes visited
         visited++;
-        j = lookup[last];   //Cerchiamo last in id_1
+        j = lookup[last];   //search for last in id_1
  
         if (first == last){
-            scambia(&parent_1, &parent_2);          //Una volta terminato il ciclo scambiamo i genitori in modo da copiare le città al contrario
-            while (flags[j]) j++;                   //Cerchiamo una colonna libera
-            first = population[id_1].solution[j];   //Reinizializziamo il primo elemento del ciclo
+            scambia(&parent_1, &parent_2);          //We use this every cycle to copy the cities in the childs in reverse order (swapping the parents)
+            while (flags[j]) j++;                   //Search for a free column
+            first = population[id_1].solution[j];   //Reinitizialize first element of the cycle
         }  
     }
 }
